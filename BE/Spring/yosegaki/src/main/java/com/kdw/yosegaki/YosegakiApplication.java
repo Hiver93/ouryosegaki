651861e9592028a7610432fed3d68cf131dcc1b2
hiver93 <kdw3238@naver.com>
2025-10-25 18:32:38 +0900

    [init] BE-init

---
+++ BE/Spring/yosegaki/src/main/java/com/kdw/yosegaki/YosegakiApplication.java
@@ -0,0 +1,13 @@
+package com.kdw.yosegaki;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YosegakiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YosegakiApplication.class, args);
+	}
+
+}
---
+++ BE/Spring/yosegaki/src/test/java/com/kdw/yosegaki/YosegakiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kdw.yosegaki;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YosegakiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
