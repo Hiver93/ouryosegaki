7c78f03af7a360a23fe95629af57fe6bfd428233
hiver93 <kdw3238@naver.com>
2025-10-26 22:50:07 +0900

    [chore] FE-changing url

--- FE/ouryosegaki/src/api/http.ts
+++ FE/ouryosegaki/src/api/http.ts
@@ -5,8 +5,8 @@ import type { AxiosRequestConfig } from "axios";
 // localServer 통신
 function localServer() {
   const axiosConfig: AxiosRequestConfig = {
-    // baseURL: "http://43.207.181.178:8080/", // 직접 URL 지정
-    baseURL: "http://localhost:8080/", // 직접 URL 지정
+    baseURL: "http://43.207.181.178:8080/", // 직접 URL 지정
+    // baseURL: "http://localhost:8080/", // 직접 URL 지정
     headers: {
       "Content-Type": "application/json;charset=utf-8",
     },
